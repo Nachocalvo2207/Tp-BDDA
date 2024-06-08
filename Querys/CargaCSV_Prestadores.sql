8045b24fe85d084f5f6fe2a39baf04b7b8326ae5
Ignaciocalvo22 <nachocalvo22@gmail.com>
2024-06-07 22:31:17 -0300

    Create SP CargaCSV_Prestadores

---
+++ Querys/CargaCSV_Prestadores.sql
@@ -0,0 +1,53 @@
+CREATE OR ALTER PROCEDURE Clinica.importarPrestadores
+@rutaArchivo NVARCHAR(MAX)
+AS
+BEGIN
+    DECLARE @sql NVARCHAR(MAX)
+
+    --Chequeamos si la tabla temporal existe, si existe la borramos
+    DROP TABLE IF EXISTS clinica.#tempPrestador
+    CREATE TABLE clinica.#tempPrestador
+    (
+        Nombre_Prestador VARCHAR(50)
+        ,Plan_prestador VARCHAR(50)
+    )
+    --Corro el BULK del import
+    DECLARE @ImportPrestadores NVARCHAR(MAX)
+    SET @ImportPrestadores = 
+    'BULK INSERT clinica.#tempPrestador ' +
+    'FROM ''' + @rutaArchivo + ''' ' +
+    'WITH ( ' +
+    '    FIELDTERMINATOR = '';'', ' +
+    '    ROWTERMINATOR = ''\n'', ' +
+    '    FIRSTROW = 2, ' +
+    '    CODEPAGE = ''65001''' +
+    ')'
+
+   EXEC sp_executesql @ImportPrestadores
+
+
+
+   --Limpio los datos de la tabla STG quitando los ";;" del final de la linea
+    UPDATE clinica.#tempPrestador
+    SET Plan_prestador = LEFT(Plan_prestador, LEN(Plan_prestador) - 2)
+    WHERE Plan_prestador LIKE '%;;'
+
+
+
+--Inserto los datos nuevos en la tabla final:
+
+--Inserto los datos nuevos en la tabla final, evitando duplicados:
+INSERT INTO clinica.Prestador
+(
+    Nombre_Prestador
+    ,Plan_prestador
+)
+SELECT
+    Nombre_Prestador
+    ,Plan_prestador
+FROM clinica.#tempPrestador A
+WHERE a.Nombre_Prestador 
+NOT IN (SELECT Nombre_Prestador FROM clinica.Prestador
+        WHERE Nombre_Prestador = a.Nombre_Prestador AND Plan_prestador = a.Plan_prestador)
+
+END
