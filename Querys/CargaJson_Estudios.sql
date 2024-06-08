b8dc1afcc41bec5416c4a758b9ffb54c35effce3
Federico Rossendy <fede07@users.noreply.github.com>
2024-06-07 23:11:15 -0300

    JSON

---
+++ Querys/CargaJson_Estudios.sql
@@ -0,0 +1,66 @@
+USE Com2900G03
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'clinicaImportar')
+    EXEC('CREATE SCHEMA clinicaImportar')
+GO
+
+CREATE PROCEDURE Clinica.ImportarEstudios
+    @rutaArchivo NVARCHAR(MAX)
+AS
+BEGIN
+
+    DROP TABLE IF EXISTS clinica.tempEstudio
+    CREATE TABLE clinica.tempEstudio
+    (
+        Id_Estudio VARCHAR(50) PRIMARY KEY,
+        Area VARCHAR(50),
+        Nombre_Estudio VARCHAR(50),
+        Prestador VARCHAR(50),
+        Plan_ VARCHAR(50),
+        Conbertura INT,
+        Costo INT,
+        Autorizacion VARCHAR(50),
+    )
+    
+    INSERT INTO clinica.tempEstudio
+    (
+        Id_Estudio,
+        Area,
+        Nombre_Estudio,
+        Prestador,
+        Plan_,
+        Conbertura,
+        Costo,
+        Autorizacion
+    )
+
+    SELECT
+        Id_Estudio,
+        Area,
+        Nombre_Estudio,
+        Prestador,
+        Plan_,
+        Conbertura,
+        Costo,
+        Autorizacion
+    FROM OPENROWSET
+    (
+        BULK 'C:\Users\fede0\Desktop\BDDA\Tp-BDDA\Tp-BDDA\Dataset\Centro_Autorizaciones.Estudios clinicos.json',
+        Single_Clob
+    )
+    AS Estudios
+    CROSS APPLY OPENJSON(BulkColumn)
+    WITH
+    (
+        Id_Estudio VARCHAR(50) '$._id."$oid"',
+        Area VARCHAR(50) '$.Area',
+        Nombre_Estudio VARCHAR(50) '$.Estudio',
+        Prestador VARCHAR(50) '$.Prestador',
+        Plan_ VARCHAR(50) '$.Plan',
+        Conbertura INT '$."Porcentaje Cobertura"',
+        Costo INT '$.Costo',
+        Autorizacion VARCHAR(50) '$."Requiere Autorizacion"'
+    )
+
+END
