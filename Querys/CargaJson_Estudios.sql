0de6dcf074658f3d4eb94782db4d39b2cd8325bc
Federico Rossendy <fede07@users.noreply.github.com>
2024-06-07 23:38:11 -0300

    json

--- Querys/CargaJson_Estudios.sql
+++ Querys/CargaJson_Estudios.sql
@@ -5,6 +5,9 @@ IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'clinicaImportar')
     EXEC('CREATE SCHEMA clinicaImportar')
 GO
 
+DROP PROCEDURE IF EXISTS clinica.ImportarEstudios;
+GO
+
 CREATE PROCEDURE Clinica.ImportarEstudios
     @rutaArchivo NVARCHAR(MAX)
 AS
@@ -18,49 +21,60 @@ BEGIN
         Nombre_Estudio VARCHAR(50),
         Prestador VARCHAR(50),
         Plan_ VARCHAR(50),
-        Conbertura INT,
+        Cobertura INT,
         Costo INT,
         Autorizacion VARCHAR(50),
     )
+
+    DECLARE @sql NVARCHAR(MAX) = 
+
     
-    INSERT INTO clinica.tempEstudio
+    'INSERT INTO clinica.tempEstudio
     (
         Id_Estudio,
         Area,
         Nombre_Estudio,
         Prestador,
         Plan_,
-        Conbertura,
+        Cobertura,
         Costo,
         Autorizacion
     )
-
+    
     SELECT
         Id_Estudio,
         Area,
         Nombre_Estudio,
         Prestador,
         Plan_,
-        Conbertura,
+        Cobertura,
         Costo,
         Autorizacion
+    
     FROM OPENROWSET
     (
-        BULK 'C:\Users\fede0\Desktop\BDDA\Tp-BDDA\Tp-BDDA\Dataset\Centro_Autorizaciones.Estudios clinicos.json',
+        BULK''' + @rutaArchivo + ''',
         Single_Clob
     )
     AS Estudios
     CROSS APPLY OPENJSON(BulkColumn)
     WITH
     (
-        Id_Estudio VARCHAR(50) '$._id."$oid"',
-        Area VARCHAR(50) '$.Area',
-        Nombre_Estudio VARCHAR(50) '$.Estudio',
-        Prestador VARCHAR(50) '$.Prestador',
-        Plan_ VARCHAR(50) '$.Plan',
-        Conbertura INT '$."Porcentaje Cobertura"',
-        Costo INT '$.Costo',
-        Autorizacion VARCHAR(50) '$."Requiere Autorizacion"'
-    )
+        Id_Estudio VARCHAR(50) ''$._id."$oid"'',
+        Area VARCHAR(50) ''$.Area'',
+        Nombre_Estudio VARCHAR(50) ''$.Estudio'',
+        Prestador VARCHAR(50) ''$.Prestador'',
+        Plan_ VARCHAR(50) ''$.Plan'',
+        Cobertura INT ''$."Porcentaje Cobertura"'',
+        Costo INT ''$.Costo'',
+        Autorizacion VARCHAR(50) ''$."Requiere Autorizacion"''
+    )'
 
+    EXEC sp_executesql @sql
 END
+GO
+
+-- Test
+
+EXEC Clinica.ImportarEstudios 'C:\Users\fede0\Desktop\BDDA\Tp-BDDA\Tp-BDDA\Dataset\Centro_Autorizaciones.Estudios clinicos.json'
+
